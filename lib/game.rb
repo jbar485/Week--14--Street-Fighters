710561e16ef93b0e8ee6493b770fbe0963b0c749
Student <example@example.com>
2020-02-03 12:04:15 -0800

    add ken

--- lib/game.rb
+++ lib/game.rb
@@ -6,15 +6,29 @@ module FightingGame
       self.caption = 'Figther'
       @backdrop  = Backdrop.new(self, "battlebrawl_backdrop.png")
 
-      @player1 = Player.new(self,"character1")
+      @player1 = Player.new(self,"ryu", false)
+      @player2 = Player.new(self, "ken", true)
+
+      @controls1 = Controls.new(self, @player1, 1)
+      @controls2 = Controls.new(self, @player2, 2)
     end
 
-    def update
-    end
 
     def draw
       @backdrop.draw
+      @player1.move_to 100
+      @player2.move_to width - 100 - @player2.width
       @player1.draw
+      @player2.draw
+    end
+
+    def update
+    @controls1.update 0, @player2.left
+    @controls2.update @player1.right, width
+    end
+
+    def button_down?(char)
+      super char_to_button_id(char)
     end
 
   end
--- lib/player.rb
+++ lib/player.rb
@@ -1,19 +1,49 @@
 module FightingGame
 
 class Player
-  SCALE = 5
-  POS_Y = -150
+  SCALE = 3
+  POS_Y = 180
+  SPEED = 3
 
-  def initialize(window, name)
-    @image = Gosu::Image.new(window, "assets/#{name}/idle-1.gif")
-    # move_to 0
+  def initialize(window, name, flip=false)
+    @images = (1...4).map do |i|
+    Gosu::Image.new(window, "assets/#{name}/idle-#{i}.gif", false)
+  end
+    @pos_x = 0
+    @flip = flip
+    @max_x = window.width
+    move_to flip ? @max_x - 100 - width : 100
   end
 def move_to(x)
   @pos_x = x
 end
 
+def width
+  @images[0].width * SCALE
+end
+
+def left
+  @pos_x
+end
+
+def right
+  @pos_x + width
+end
+
+
+def move_left
+      @pos_x -= SPEED
+    end
+
+    def move_right
+      @pos_x += SPEED
+    end
+
   def draw
-    @image.draw( 100, POS_Y, 1, SCALE, SCALE)
+    pos_x = @pos_x + (@flip ? width : 0)
+    scale_x = SCALE * (@flip? -1 : 1)
+    image = @images[Gosu.milliseconds / 200 % @images.size]
+    image.draw(pos_x, POS_Y, 1, scale_x, SCALE)
   end
 end
 
